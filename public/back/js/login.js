48ba03f6dbc5e0f35f803420ab6c8bb3702424b0
amandagithub107 <898138418@qq.com>
2018-05-09 22:20:49 +0800

    登陆校验功能基本完成

---
+++ public/back/js/login.js
@@ -0,0 +1,39 @@
+
+$(function () {
+  $('#form').bootstrapValidator({
+  
+    feedbackIcons: {
+      valid: 'glyphicon glyphicon-ok',
+      invalid: 'glyphicon glyphicon-remove',
+      validating: 'glyphicon glyphicon-refresh'
+    },
+    
+    fields: {
+      username: {
+        validators: {
+          notEmpty: {
+            message: "用户名不能为空"
+          },
+          stringLength: {
+            min: 2,
+            max: 6,
+            message: "用户名必须是2-6位"
+          }
+        }
+      },
+      password: {
+        validators: {
+          notEmpty: {
+            message: "密码不能为空"
+          },
+          stringLength: {
+            min: 6,
+            max: 12,
+            message: "密码长度必须在6-12位"
+          }
+        }
+      }
+      
+    }
+  });
+})
