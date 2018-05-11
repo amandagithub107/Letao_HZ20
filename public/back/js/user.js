b870da81b14f56549ad55c31260598ea132e6609
amandagithub107 <898138418@qq.com>
2018-05-11 21:54:49 +0800

    分页渲染完成

--- public/back/js/user.js
+++ public/back/js/user.js
@@ -7,22 +7,51 @@ $(function () {
   var pageSize = 5;
   
   
-  
+  //1. 一进入页面，渲染页面
+  render();
   // 向后退请求用户列表数据，通过模板引擎，进行渲染
   //后台的响应头中，设置了content-type为application/json，jquery会自动按照json格式进行解析响应结果，我们就可以不用配置dataType了
-  $.ajax({
-    type: "get",
-    url: "/user/queryUser",
-    data:{
-      page: currentPage,
-      pageSize: pageSize
-    },
-    dataType: "json",
-    success: function (info) {
-      // console.log(info);
-      var htmlStr = template("tpl", info);
-      $('.lt_content tbody').html( htmlStr );
-    }
-  })
+ function render() {
+   $.ajax({
+     type: "get",
+     url: "/user/queryUser",
+     data:{
+       page: currentPage,
+       pageSize: pageSize
+     },
+     dataType: "json",
+     success: function (info) {
+       // console.log(info);
+       var htmlStr = template("tpl", info);
+       //进行渲染
+       $('.lt_content tbody').html( htmlStr );
+      
+       //配置分页插件
+       $('#paginator').bootstrapPaginator({
+         //配置bootstrap版本
+         bootstrapMajorVersion: 3,
+         totalPages: Math.ceil(info.total / info.size),
+         currentPage: info.page,
+         //页码点击事件
+         onPageClicked: function (a, b, c, page) {
+           // 打印参数列表
+           // console.log(arguments);
+          
+           //更新当前页
+           currentPage = page;
+          
+           //重新渲染
+           render();
+         }
+       })
+     }
+   })
+ }
+  
+  
+  
+  
+  
+  
   
 })
